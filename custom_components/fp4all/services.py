360b4f8474c23f384c125565be6fc8ac0891902c
esbart1 <101978607+esbart1@users.noreply.github.com>
2026-09-23 13:16:16 +0200

    Fix history service handler

--- custom_components/fp4all/services.py
+++ custom_components/fp4all/services.py
@@ -14,6 +14,7 @@ import logging
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .history import download_history
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -39,7 +40,7 @@ async def async_register_services(hass: HomeAssistant) -> None:
                 coordinator.host,
             )
 
-            await coordinator.async_update_history()
+            await download_history(coordinator)
 
         _LOGGER.warning("FP4All service gereed")
 
